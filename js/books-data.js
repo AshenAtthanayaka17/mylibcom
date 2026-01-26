0bc8b1326cb639097190399437e8a246156022a0
Administration <156107869+AshenAtthanayaka17@users.noreply.github.com>
2026-01-26 18:58:48 +0000

    Add files via upload

---
+++ js/books-data.js
@@ -0,0 +1,18 @@
+export const books = [
+    {
+        id: 1,
+        title: "",
+        author: "",
+        genre: "",
+        year: "",
+        rating: 0,
+        category: "",
+        pages: 0,
+        location: "",
+        description: "",
+        image: "",
+        available: true,
+        status: "new" // new arrival: new, Leaving soon: leaving, or null
+    }
+
+];
