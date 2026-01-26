05222477fd91085658655f1e5fd51199a26565fb
Administration <156107869+AshenAtthanayaka17@users.noreply.github.com>
2026-01-27 00:37:16 +0530

    Update books-data.js

--- js/books-data.js
+++ js/books-data.js
@@ -1,18 +1,19 @@
 export const books = [
     {
         id: 1,
-        title: "",
-        author: "",
-        genre: "",
+        title: "Mandodari | මන්දෝදරී",
+        author: "Mohan Raj Mdawala",
+        genre: "Novel",
         year: "",
-        rating: 0,
-        category: "",
-        pages: 0,
-        location: "",
-        description: "",
+        rating: 1,
+        category: "Adult",
+        pages: 264,
+        location: "SKU 9786246039004",
+        description: "ඒ සමඟ ඇයට ඇති වූ කේන්තිය කෙතරම්ද යත් ඇය මිට මොළවා ඔහුට පහර දුන්නාය. ගැනු පළිගන්න හැටි ඔයා තාම දන්නෙ නැ.. ඇය එසේ කීවේ ඔහු ඇස් පියා ඒ වේදනාව විඳින අතරය.ඉන්පසුව යළිත් ඔහුගේ පපුව මතම හිස හොවා මෙසේද කීවාය. ඒ පළිගන්න මිනිස්සුන්ගේ පපුව උඩම තමා ගෑනු ඔලුව තියාගන්නත් කැමති.! This is just a review about this book ‘Mandodari’ , after 3hours of reading that book it makes me feel that this author written this book while he was high (after using some kind of drug) his way of writing is poor and have no literature at all. It just a book with lot lush like a dumb story in a pornoghraphy video. I did my best to analyse the story until the end to realise that the girl on this book was a schizophrenia patient, but overall this story does not have any wise meaning or anything rather than dumb horny emotions and narrow idea of sex , love and heating!",
         image: "",
         available: true,
         status: "new" // new arrival: new, Leaving soon: leaving, or null
     }
+
 
 ];
