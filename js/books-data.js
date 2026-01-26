203a0db7315c5170c56306525bdd66e0938a88af
Administration <156107869+AshenAtthanayaka17@users.noreply.github.com>
2026-01-27 00:43:54 +0530

    Modify book entry for 'Mandodari'
    Updated the book description and added an image link.

--- js/books-data.js
+++ js/books-data.js
@@ -9,11 +9,12 @@ export const books = [
         category: "Adult",
         pages: 264,
         location: "SKU 9786246039004",
-        description: "ඒ සමඟ ඇයට ඇති වූ කේන්තිය කෙතරම්ද යත් ඇය මිට මොළවා ඔහුට පහර දුන්නාය. ගැනු පළිගන්න හැටි ඔයා තාම දන්නෙ නැ.. ඇය එසේ කීවේ ඔහු ඇස් පියා ඒ වේදනාව විඳින අතරය.ඉන්පසුව යළිත් ඔහුගේ පපුව මතම හිස හොවා මෙසේද කීවාය. ඒ පළිගන්න මිනිස්සුන්ගේ පපුව උඩම තමා ගෑනු ඔලුව තියාගන්නත් කැමති.! This is just a review about this book ‘Mandodari’ , after 3hours of reading that book it makes me feel that this author written this book while he was high (after using some kind of drug) his way of writing is poor and have no literature at all. It just a book with lot lush like a dumb story in a pornoghraphy video. I did my best to analyse the story until the end to realise that the girl on this book was a schizophrenia patient, but overall this story does not have any wise meaning or anything rather than dumb horny emotions and narrow idea of sex , love and heating!",
-        image: "",
+        description: "This is just a review about this book ‘Mandodari’ , after 4hours of reading that book it makes me feel that this author written this book while he was high (after using some kind of drug) his way of writing is poor and have no literature at all. It just a book with lot lush like a dumb story in a pornoghraphy video. I did my best to analyse the story until the end to realise that the girl on this book was a schizophrenia patient, but overall this story does not have any wise meaning or anything rather than dumb horny emotions and narrow idea of sex , love and heating!",
+        image: "https://www.honwix.com/uploads/book/cover/a9fb5fe65b31585745f2356e78c25cf3d6254f9b6ee59da5a34ad9c4d2751112.webp",
         available: true,
         status: "new" // new arrival: new, Leaving soon: leaving, or null
     }
 
 
 ];
+
